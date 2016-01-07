681e3f4ec5a8bbfe07eeaec716d24072247b2d62
TakaHaru <xt.sakamoto@gmail.com>
2016-01-07 14:40:40 +0900

    for文で６作品の表示を追記

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 
 
-<?php
+  <?php
 
   //try-catchで接続エラーを取得＆表示
   try{
@@ -36,14 +36,18 @@
   //SQLを実行
   $query -> execute();
 
-  foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $key){
-      //var_dump($key);
-      //echo $key["ImageUrl"].'<br>';
-  ?>
-  <a href="<?php echo $key["FilmUrl"];?>" target="window"> <img src="<?php echo $key["ImageUrl"];?>"></a>
-  <!-- <img src="<?php echo $key["ImageUrl"]; ?>" width="150" height="60"> -->
+      foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $key){
+        for($i=1; $i<=6; $i++){
+          if($key["FilmID"] == $i){?>
+      <a href="<?php echo $key["FilmUrl"];?>" target="window"> <img src="<?php echo $key["ImageUrl"];?>"></a>
+
   <?php
-  }
+      //var_dump($key["FilmID"]);
+          }
+        }
+      }
+
   //接続を切断
   $pdo_object = null;
+
   ?>
