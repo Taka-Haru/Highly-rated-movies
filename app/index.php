74c37c41a32ca38d86e5d7f2d811a503cedd8877
TakaHaru <xt.sakamoto@gmail.com>
2016-01-11 16:43:12 +0900

    画面デザイン5章まで

---
+++ app/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>HighlyRatedMovies</title>
+    <link rel="stylesheet" href="../common/css/normalize.css">
+    <link rel="stylesheet" href="../common/css/style.css">
+</head>
+
+<body>
+<article　class="articleDetail">
+  <header class="articleDetailHead">
+    <h1 class="pageTitle">Highly Rated Movies</h1>
+    <h3 class="pageTitleSub">〜本日おすすめの映画〜</h3>
+  </header>
+
+<figure>
+  <figcaption>今日のピックアップムービー</figcaption>
+  <img src="../image/img_01_01.jpg" alt="ランダムで映画ジャケが表示されます" width="720" height="390">
+</figure>
+
+  <section class="articleDetailBody">
+    <ul>
+      <li>あいうえお</li>
+      <li>かきくけこ</li>
+      <li>さしすせそ</li>
+      <li>たちつてと</li>
+    </ul>
+
+    <table border="1">
+      <caption>ひらがな</caption>
+      <tr>
+      <th>あ行</th>
+      <td>あいうえお</td>
+      </tr>
+
+      <tr>
+      <th>か行</th>
+      <td>かきくけこ</td>
+      </tr>
+
+      <tr>
+      <th>さ行</th>
+      <td>さしすせそ</td>
+      </tr>
+    </table>
+  </section>
+
+  <footer class="articleDetailFoot">
+    <aside>
+      <p>〜サイト作成協力〜</p>
+      <h2 class="heading-typeC">グルーヴ・ギア株式会社</h2>
+      <p>地下鉄九段下駅より徒歩１分</p>
+      <ul>
+        <li><a href="http://www.groove-gear.jp/" target="_blank">http://www.groove-gear.jp/</a></li>
+        <li><a href="https://camp.geekjob.jp/?_ga=1.84169246.649395329.1446601110" target="_blank">GeekJobCamp ページ</a></li>
+      </ul>
+      <P>是非あなたもプログラミングキャンプに参加しませんか？</p>
+    </aside>
+  </footer>
+
+</article>
+</body>
+</html>
