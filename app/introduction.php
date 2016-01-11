114cbbfb88d5a2f517f9acd05eef5f01b443305d
TakaHaru <xt.sakamoto@gmail.com>
2016-01-12 02:59:25 +0900

    自己紹介ページ

---
+++ app/introduction.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>HighlyRatedMovies</title>
+    <link rel="stylesheet" href="../common/css/normalize.css">
+    <link rel="stylesheet" href="../common/css/style.css">
+</head>
+
+<body>
+  <div id="page">
+  <header id="pageHead">
+    <h1 id="siteTitle">HighlyRatedMovies</h1>
+    <p id="catchcopy">〜今日から始める映画鑑賞〜</p>
+    <nav class="globalNavi">
+      <ul>
+        <li><a href="http://localhost/develop/HighlyRatedMovies/app/index.php">ホーム</a></li>
+        <li class="current"><a href="http://localhost/develop/HighlyRatedMovies/app/introduction.php">自己紹介</a></li>
+        <li><a href="http://localhost/develop/HighlyRatedMovies/app/explanation.php">このサイトについて</a></li>
+        <li><a href="http://localhost/develop/HighlyRatedMovies/app/index.php">お問い合わせ</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <div id="pageBody">
+
+    <div id="pageBodyMain">
+
+      <article　class="articleDetail">
+        <header class="articleDetailHead">
+          <h1 class="pageTitle">自己紹介</h1>
+        </header>
+
+        <section>
+          <h3 class="heading-typeB">〜 GeekJobCamp 第一期生 〜</h3>
+          <!-- <figure> -->
+            <img src="../image/sub21.jpg" alt="ランダムで映画ジャケが表示されます" width="720" height="390">
+              <figcaption>※上記映画のタイトル</figcaption>
+          <!-- </figure> -->
+        </section
+
+        <section class="articleDetailBody">
+          <ul>
+            <li>　2015年の11月から、GeekJobのプログラミングキャンプに参加している27歳です。</li>
+            <li>前職は外食企業で約３年半働いてました。</li>
+            <li>　キャンプ入る前までは、インターネットってよく分からないけど便利だなってレベルでした(笑)</li>
+            <li>まだまだ、プログラミングは勿論、基本的なIT知識に疎い状態で、日々勉強の毎日です。</li>
+          </ul>
+        </section>
+
+        <footer class="articleDetailFoot">
+          <aside>
+            <p>〜サイト作成協力〜</p>
+            <h2 class="heading-typeC">グルーヴ・ギア株式会社</h2>
+            <p>地下鉄九段下駅より徒歩１分</p>
+            <ul>
+              <li><a href="http://www.groove-gear.jp/" target="_blank">http://www.groove-gear.jp/</a></li>
+              <li><a href="https://camp.geekjob.jp/?_ga=1.84169246.649395329.1446601110" target="_blank">GeekJobCamp ページ</a></li>
+            </ul>
+            <P>是非あなたもプログラミングキャンプに参加しませんか？</p>
+          </aside>
+        </footer>
+      </article>
+<!-- pageBodyMainタグ終了 --></div>
+
+  <div id="pageBodySub">
+    <section class="newList">
+      <h2>おしらせ</h2>
+      <ul>
+        <li><time datetime="2016.00.00">2016.00.00</time>
+          映画データを更新しました。</li>
+        <li><time datetime="2016.00.00">2016.00.00</time>
+          サイトデザインを一部変更しました。</li>
+      </ul>
+    </section>
+<!-- pageBodySubタグ終了 --></div>
+
+<!-- pageBodyタグ終了 --></div>
+
+<!-- ページの先頭に戻る処理-->
+<p class="pagetop"><a href="#page">ページの先頭に戻る</a></p>
+
+  <footer id="pageFoot">
+    <p id="copyright"><small>Copyright&copy; 2016 @HighlyRatedMovies All Rights Reserved.</small></p>
+  </footer>
+
+
+  <!-- pageタグ終了 --></div>
+
+</body>
+</html>
