7330dc2f9f54aee43309ae896fdf172c2caa9c64
TakaHaru <xt.sakamoto@gmail.com>
2016-01-12 15:49:02 +0900

    toiawase no 問い合わせのセッション関数追記

---
+++ common/scriptUtil.php
@@ -0,0 +1,26 @@
+<?php
+
+//トップページへのリンクの関数
+function return_top(){
+    return "<a href='".ROOT_URL."'>トップページ</a>";
+}
+
+
+//セッションの値を取得する関数
+function contact_session($name){
+    if(isset($_POST['mode']) && $_POST['mode'] == 'REINPUT'){
+        if(isset($_SESSION[$name])){
+            return $_SESSION[$name];
+        }
+    }
+}
+//POSTの値をセッションに格納する関数
+function confirm_session($name){
+    if(!empty($_POST[$name])){
+        $_SESSION[$name] = $_POST[$name];
+        return $_POST[$name];
+    }else{
+        $_SESSION[$name] = null;
+        return null;
+    }
+}
