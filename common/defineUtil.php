04fada518a6a855acafe9a874aafe8bdc17b3d25
TakaHaru <xt.sakamoto@gmail.com>
2016-01-12 15:48:15 +0900

    ホームと問い合わせ項目追記

---
+++ common/defineUtil.php
@@ -0,0 +1,5 @@
+<?php
+const ROOT_URL = 'http://localhost/develop/HighlyRatedMovies/app/index.php';   //indexへのURL
+const CONTACT = 'contact.php';                                //お問い合わせページ
+const CONTACT_CONFIRM = 'contact_confirm.php';                //お問い合わせ確認ページ
+const CONTACT_RESULT = 'contact_result.php';                  //お問い合わせ結果ページ
