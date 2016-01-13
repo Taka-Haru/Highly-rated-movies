931c75f8b385d5006ee2102b1dd5b4d2f9b6534b
TakaHaru <xt.sakamoto@gmail.com>
2016-01-13 17:59:50 +0900

    ツイッターボタン追記

--- app/index.php
+++ app/index.php
@@ -50,6 +50,14 @@
   <p class="pagetop"><a href="#page">ページの先頭に戻る</a></p>
 
   <footer id="pageFoot">
+    <a href="https://twitter.com/share" class="twitter-share-button"{count}>Tweet</a>
+
+    <script>
+      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+      if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+      fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+    </script>
+    
     <p id="copyright"><small>Copyright&copy; 2016 @HighlyRatedMovies All Rights Reserved.</small></p>
   </footer>
 
