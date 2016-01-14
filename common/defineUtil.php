74e5139fd78dffca884ca6e9d749fb51bd549581
TakaHaru <xt.sakamoto@gmail.com>
2016-01-14 18:02:58 +0900

    映画詳細ページ追加

--- common/defineUtil.php
+++ common/defineUtil.php
@@ -3,3 +3,4 @@ const ROOT_URL = 'http://localhost/develop/HighlyRatedMovies/app/index.php';   /
 const CONTACT = 'contact.php';                                //お問い合わせページ
 const CONTACT_CONFIRM = 'contact_confirm.php';                //お問い合わせ確認ページ
 const CONTACT_RESULT = 'contact_result.php';                  //お問い合わせ結果ページ
+const MOVIE_DETAIL ='movie_detail.php';                       //映画詳細ページ
